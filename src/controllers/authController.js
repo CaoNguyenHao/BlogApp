51e30eae059c90333b0fcd0b4ac98acf56486e38
Hao CN <caonguyenhao69@gmail.com>
2025-06-30 09:44:54 +0700

    Add authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,23 @@
-import User from '../models/user.js';
+import bcrypt from 'bcrypt';
+import { User } from '../models/user.js';
 
 export const getLogin = (req, res) => {
-    res.render('login', { error: null });
+    res.render('auth/login', { error: null });
 };
 
 export const postLogin = async (req, res) => {
     const { email, password } = req.body;
+
     const user = await User.findOne({ where: { email } });
 
-    if (!user || !(await user.validPassword(password))) {
-        return res.render('login', { error: 'Sai email hoặc mật khẩu' });
+    if (!user) {
+        return res.render('auth/login', { error: 'Wrong email or password' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.render('auth/login', { error: 'Wrong email or password' });
     }
 
     req.session.user = {
@@ -18,17 +26,18 @@ export const postLogin = async (req, res) => {
         role: user.role
     };
 
-    res.redirect('/dashboard');
+    return res.redirect('/posts');
 };
 
+
 export const getRegister = (req, res) => {
-    res.render('register', { error: null });
+    res.render('auth/register', { error: null });
 };
 
 export const postRegister = async (req, res) => {
     const { name, email, password } = req.body;
     const existed = await User.findOne({ where: { email } });
-    if (existed) return res.render('register', { error: 'Email đã tồn tại' });
+    if (existed) return res.render('auth/register', { error: 'Email already exists' });
 
     await User.create({ name, email, password });
     res.redirect('/login');
